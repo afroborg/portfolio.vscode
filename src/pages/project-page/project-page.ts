b74f05b81056a39f004299682ccbc62dfd1a8dda
axel <axel.froborg@gmail.com>
2020-08-14 09:29:07 +0200

    reload page on route change

--- src/pages/project-page/project-page.ts
+++ src/pages/project-page/project-page.ts
@@ -1,30 +1,41 @@
 import icon from '@/components/icon/icon.vue';
 import { toDaysSince } from '@/helpers/date-helpers';
 import { getProjectDetails } from '@/helpers/projects-helpers';
+import { IProject } from '@/models/IProject';
 import { defineComponent } from 'vue';
-import { useRoute } from 'vue-router';
+import { NavigationGuardNext, RouteLocation, useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'project-page',
   components: {
     icon
   },
+  async beforeRouteUpdate(to: RouteLocation, from: RouteLocation, next: NavigationGuardNext) {
+    if (to.params.id !== from.params.id) {
+      await this.getProjectData(to.params.id.toString());
+      next();
+    }
+    next();
+  },
   setup() {
     const route = useRoute();
     return {
-      route
+      route,
     };
   },
   data: () => ({
     project: {}
   }),
   mounted() {
-    this.getProjectData();
+    this.getProjectData(this.route.params.id.toString());
   },
   methods: {
-    async getProjectData() {
-      const data = await getProjectDetails(this.route.params.id.toString());
-      this.project = { ...data };
+    setData(project: IProject) {
+      this.project = { ...project };
+    },
+    async getProjectData(id: string) {
+      const data = await getProjectDetails(id);
+      this.setData(data);
     },
     toDaysSince
   }
