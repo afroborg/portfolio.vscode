9283d688ea844fe76345a8868baf828930f3263d
axel <axel.froborg@gmail.com>
2020-08-18 12:40:39 +0200

    settings done

---
+++ src/components/layout/page-sidebar/page-settings/page-settings-config.ts
@@ -0,0 +1,20 @@
+export const CONFIG = {
+  THEMES: [
+    {
+      key: 'dark',
+      value: 'Dark',
+    },
+    {
+      key: 'light',
+      value: 'Light',
+    },
+    {
+      key: 'material-dark',
+      value: 'Material dark',
+    },
+    {
+      key: 'material-light',
+      value: 'Material light',
+    }
+  ]
+};
--- src/components/layout/page-sidebar/page-settings/page-settings.ts
+++ src/components/layout/page-sidebar/page-settings/page-settings.ts
@@ -3,9 +3,11 @@ import vSelect from '@/components/v-select/v-select.vue';
 import { addNotification } from '@/helpers/notification-helpers';
 import { capitalize } from '@/helpers/string-helpers';
 import { INotification } from '@/models/INotification';
-import { IOption } from '@/models/IOption';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useStore } from 'vuex';
+import { CONFIG } from './page-settings-config';
+
+
 
 export default defineComponent({
   name: 'page-settings',
@@ -16,55 +18,44 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const add = (notification: INotification) => addNotification(notification, store);
+
     return {
-      add
+      add,
+      ...useThemes({ add }),
     };
   },
-  data: () => ({
-    theme: 'dark',
-    themes: [
-      {
-        key: 'dark',
-        value: 'Dark',
-      },
-      {
-        key: 'light',
-        value: 'Light',
-      },
-      {
-        key: 'material-dark',
-        value: 'Material dark',
-      },
-      {
-        key: 'material-light',
-        value: 'Material light',
-      },
-    ] as IOption[]
-  }),
-  mounted() {
-    const theme = localStorage.getItem('theme') || 'dark';
-    this.setTheme(theme, false);
-
-    // Theme switcher with alt+t
-    document.addEventListener('keydown', (e: KeyboardEvent) => {
-      if (e?.altKey && e?.key?.toLowerCase() === 't') {
-        const currentThemeIndex = this.themes.findIndex(t => t.key === this.theme);
-        const nextIndex = currentThemeIndex + 1 > this.themes.length - 1 ? 0 : currentThemeIndex + 1;
-        this.setTheme(this.themes[nextIndex].key);
-      }
-    });
-  },
-  methods: {
-    setTheme(key: string, showNotification = true) {
-      const theme = this.themes.find(t => t.key === key);
-      if (theme) {
-        document.documentElement.setAttribute('data-theme', key);
-        localStorage.setItem('theme', key);
-        this.theme = key;
-
-        if (showNotification)
-          this.add({ header: 'Theme updated', body: `${capitalize(theme.value)} is now the active theme`, type: 'success' });
-      }
-    }
-  },
 });
+
+const useThemes = (addons: any) => {
+  const theme = ref(localStorage.getItem('theme') || 'dark');
+  const themes = CONFIG.THEMES;
+
+  const setTheme = (key: string, showNotification = true) => {
+    const themeToSet = themes.find(t => t.key === key);
+    if (themeToSet) {
+      document.documentElement.setAttribute('data-theme', key);
+      localStorage.setItem('theme', key);
+      theme.value = key;
+
+      if (showNotification)
+        addons.add({ header: 'Theme updated', body: `${capitalize(themeToSet.value)} is now the active theme`, type: 'success' });
+    }
+  };
+
+  // Theme switcher with alt+t
+  document.addEventListener('keydown', (e: KeyboardEvent) => {
+    if (e?.altKey && e?.key?.toLowerCase() === 't') {
+      const currentThemeIndex = themes.findIndex(t => t.key === theme.value);
+      const nextIndex = currentThemeIndex + 1 > themes.length - 1 ? 0 : currentThemeIndex + 1;
+      setTheme(themes[nextIndex].key);
+    }
+  });
+
+  setTheme(theme.value, false);
+
+  return {
+    theme,
+    themes,
+    setTheme
+  };
+};
