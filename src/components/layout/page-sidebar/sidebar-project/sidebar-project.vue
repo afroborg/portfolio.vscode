1917b4a45523bd5c01edbd2b69855c6674a84cba
axel <axel.froborg@gmail.com>
2020-08-18 13:36:27 +0200

    limited data returned by /projects

--- src/components/layout/page-sidebar/sidebar-project/sidebar-project.vue
+++ src/components/layout/page-sidebar/sidebar-project/sidebar-project.vue
@@ -3,7 +3,7 @@
     <div :class="`projects-list__item ${expanded ? 'projects-list__item--expanded' : ''}`">
       <div class="projects-list__item__header" @click="toggle">
         <i class="fal fa-angle-right" />
-        <icon v-if="tags.length" :name="tags[0]" />
+        <icon v-if="languages.length" :name="languages[0]" />
         <span>{{ name }}</span>
       </div>
 
@@ -12,17 +12,17 @@
           <span>readme.md</span>
         </router-link>
         <a
-          :href="!github || isPrivate ? '#' : `https://github.com/${github.user}/${github.repository}`"
+          :href="!github ? '#' : `https://github.com/${github.user}/${github.repository}`"
           class="projects-list__item__content__item"
-          :class="[(!github || isPrivate) && 'projects-list__item__content__item--disabled']"
+          :class="[!github && 'projects-list__item__content__item--disabled']"
           target="_blank"
           rel="norefferer">
           <span>github.html</span>
           <i v-if="!github || isPrivate" class="fal fa-lock-alt" />
         </a>
-        <a :href="!demo ? '#' : demo" class="projects-list__item__content__item" :class="[!demo && 'projects-list__item__content__item--disabled']" target="_blank" rel="norefferer">
+        <a :href="!demoUrl ? '#' : demoUrl" class="projects-list__item__content__item" :class="[!demoUrl && 'projects-list__item__content__item--disabled']" target="_blank" rel="norefferer">
           <span>demo.html</span>
-          <i v-if="!demo" class="fal fa-lock-alt" />
+          <i v-if="!demoUrl" class="fal fa-lock-alt" />
         </a>
       </div>
     </div>
--- src/models/IProject.ts
+++ src/models/IProject.ts
@@ -3,6 +3,8 @@ import { IProjectGithubContent, IProjectGitub } from './IGithub';
 export interface IProject {
   id: string;
   name: string;
+  description: string;
+  demoUrl?: string;
   tags: string[];
   languages: string[];
   isPrivate: boolean;
