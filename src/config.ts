a036e01514e5a63fa1ceab472f8068f61f01e62b
Axel Froborg <axel.froborg@gmail.com>
2020-11-02 16:36:58 +0100

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
 export const config = {
-  API_URL:
-    process.env.NODE_ENV !== 'production'
-      ? 'https://axel-portfolio-api.azurewebsites.net/api/'
-      : 'http://localhost:5000/api/',
+  API_URL: 'https://axel-portfolio-api.azurewebsites.net/api/',
 };
